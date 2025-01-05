405e67fc4a64def2a867358fa70306af3f6e31c6
olegius88 <sh.oleg@list.ru>
2025-01-05 18:21:14 +0500

    отмена

--- magic-globals.js
+++ magic-globals.js
@@ -19,22 +19,22 @@ Object.defineProperty(global, '__stack', {
 /** returns line number when placing this in your code: __line */
 Object.defineProperty(global, '__line', {
   get: function(){
-    return __stack[1]?.getLineNumber();
+    return __stack[1].getLineNumber();
   }
 });
 
 /** return filename (without directory path or file extension) when placing this in your code: __file */
 Object.defineProperty(global, '__file', {
   get: function(){
-    var file_pieces = __stack[1]?.getFileName()?.split(/[\\\/]/)?.slice(-1)[0]?.split('.');
-    return file_pieces?.slice(0, file_pieces.length - 1)?.join('.');
+    var file_pieces = __stack[1].getFileName().split(/[\\\/]/).slice(-1)[0].split('.');
+    return file_pieces.slice(0, file_pieces.length - 1).join('.');
   }
 });
 
 /** return file extension (without preceding period) when placing this in your code: __ext */
 Object.defineProperty(global, '__ext', {
   get: function(){
-    return __stack[1]?.getFileName()?.split('.')?.slice(-1)[0];
+    return __stack[1].getFileName().split('.').slice(-1)[0];
   }
 });
 
@@ -64,7 +64,7 @@ Object.defineProperty(global, '__fili', {
       filid = global.__filid;
     }
 
-    return __stack[1]?.getFileName() + filid + __stack[1]?.getLineNumber();
+    return __stack[1].getFileName() + filid + __stack[1].getLineNumber();
   }
 });
 
